a01917d89602f35d6ca22eb8b2865c667ab9e218
Sergey <chubssand@gmail.com>
2023-02-02 15:25:48 +0200

    script added

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,21 @@
 const hamburger = document.querySelector(".hamburger"),
     menu = document.querySelector(".menu"),
     close = document.querySelector(".menu__close");
-    block = document.querySelector(".menu__block");
+block = document.querySelector(".menu__block");
 
 hamburger.addEventListener('click', () => {
     menu.classList.add("active");
     block.classList.add("active");
 });
 
-close.addEventListener('click', () =>{
+close.addEventListener('click', () => {
     menu.classList.remove('active');
     block.classList.remove("active");
+});
+
+const percent = document.querySelectorAll('.scale__pers'),
+    lines = document.querySelectorAll('.scale__line span');
+
+percent.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
 });
